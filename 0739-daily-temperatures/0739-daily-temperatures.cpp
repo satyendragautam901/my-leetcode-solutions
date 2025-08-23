9f73335b714cd582a6036734bb67888b5fcea6fb
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2025-08-23 18:34:27 +0530

    Time: 27 ms (38.65%), Space: 107.3 MB (39.36%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> st; // will store indices
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                int idx = st.top();
+                st.pop();
+                ans[idx] = i - idx; // formula for distance to warmer day
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
