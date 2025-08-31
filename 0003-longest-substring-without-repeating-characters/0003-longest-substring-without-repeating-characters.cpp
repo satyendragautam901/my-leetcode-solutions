f9b0b594bc5fcaef758e0d988987c3fa65ba8328
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2025-08-31 12:44:08 +0530

    Time: 3 ms (83.11%), Space: 11.7 MB (78.39%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,31 +2,25 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size(); // size of string
-        if(n==0){
+
+        if (n == 0) {// if size is 0 then return
             return 0;
         }
-        int temp = 1;
+        vector<int>lastIndex(256,-1); // holds last index of each char
+        int maxLength = 0; // holds max length 
+        int left = 0; // window left end is here
 
-        for(int i = 0; i<n-1; i++){
-        vector<bool>occur(256,false);
-        occur[s[i]] = true;
+        for(int i = 0; i<n; i++){
+            // agar char pehle aa chuka hai
+            if(lastIndex[s[i]] >=left){
+                left = lastIndex[s[i]] + 1; // left window starts from next onwards
 
-        string ans = "";
-        ans+=s[i];
-        for(int j =i+1; j<n; j++){
-            if(occur[s[j]]==true){
-                break;
             }
-            else{
-                ans+=s[j];
-                occur[s[j]]=true;
-                
-            }
-        }
-        temp = max(temp, int(ans.length()));
             
+            lastIndex[s[i]] = i; // push the index that is visited
+            maxLength = max(maxLength, i - left + 1 );
         }
 
-        return temp;
+        return maxLength;
     }
 };
