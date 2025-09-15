f4ce1f0b012d443fce51103a7740f47c06c91c28
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2025-09-15 15:05:35 +0530

    Time: 0 ms (100%), Space: 9.8 MB (16.28%) - LeetHub

---
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        vector<string>str; // keep the words of each word in text
+        stringstream ss(text);
+        // cout<<ss.str(); // print the string
+        string word;
+
+        while(ss>>word){ // har ek word ke liye, separate the word by spaces
+            str.push_back(word);
+        }
+        
+        // now str array have each words 
+
+        int ans = 0;
+        for (int i = 0; i < str.size(); i++) {
+            bool valid = true; // assume word is valid
+
+            // check har character of word
+            for (int k = 0; k < str[i].size(); k++) {
+                for (int j = 0; j < brokenLetters.size(); j++) {
+                    if (str[i][k] == brokenLetters[j]) {
+                        valid = false; // agar broken letter mila, word invalid
+                        break;
+                    }
+                }
+                if (!valid) break; // ek bhi broken mila toh aage check nhi karna
+            }
+
+            if (valid) {
+                ans++; // word valid hai toh count me add
+            }
+        }
+
+        return ans;
+    }
+};
