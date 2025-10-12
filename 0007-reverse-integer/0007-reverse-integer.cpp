032d5b879c3f99595cedd2c9dcfc2e05eb2e92e0
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2025-10-12 13:16:37 +0530

    Time: 2 ms (39.38%), Space: 8.6 MB (20.48%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int reverse(int x) {
-        string str = to_string(x);
-        int start = (str[0] == '-') ? 1 : 0; // if the first '-' then skip this 
-        int end = str.size()-1;
-        while(start < end){
-            swap(str[start++], str[end--]);
+        long long ans = 0;
+
+        while(x!=0){
+            ans = ans * 10 + x % 10;
+            x = x/10;
         }
-
-        long ans = stol(str); 
         if (ans > INT_MAX || ans < INT_MIN) return 0; 
-
-        return (int)ans;
+        return ans;
 
     }
 };
