911f498b500c3d054eaa41b0651129dd61aaec47
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2025-10-28 14:14:29 +0530

    Time: 1153 ms (50.8%), Space: 0B (100%) - LeetHub

---
+++ 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+select U.unique_id , E.name from Employees as E left join EmployeeUNI as U on E.id = U.id order by unique_id asc;
