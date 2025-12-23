d6aced487679539321069c990c99e66333fd409f
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2025-12-23 16:53:39 +0530

    Time: 0 ms (100%), Space: 15.6 MB (17.05%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        int n = strs.size();         // number of rows
-        int m = strs[0].size();      // number of columns
-        int count = 0;
+        int n = strs.size();
+        int m = strs[0].size();
+        int ans = 0;
         
-        // Check each column
-        for (int col = 0; col < m; col++) {
-            // Check if this column is NOT sorted
-            for (int row = 1; row < n; row++) {
-                // If current character is smaller than previous in this column
-                if (strs[row][col] < strs[row - 1][col]) {
-                    count++;
-                    break;  // No need to check further in this column
+        for (int j = 0; j < m; j++) {
+            for (int i = 1; i < n; i++) {
+                if (strs[i][j] < strs[i-1][j]) {
+                    ans++;
+                    break;  // Important: stop checking this column
                 }
             }
         }
         
-        return count;
+        return ans;
     }
 };
