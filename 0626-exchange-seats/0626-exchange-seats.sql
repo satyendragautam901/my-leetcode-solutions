698cff48f1563c67d6efabd45e0aa749cf98a1dc
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2026-01-12 13:32:06 +0530

    Time: 476 ms (7.07%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 SELECT 
     CASE 
-        WHEN mod(id,2) = 0 THEN id - 1   -- even id → previous position
-        WHEN mod(id,2) = 1 AND id + 1 <= (SELECT COUNT(*) FROM Seat) 
+        WHEN id % 2 = 0 THEN id - 1   -- even id → previous position
+        WHEN id % 2 = 1 AND id + 1 <= (SELECT COUNT(*) FROM Seat) 
             THEN id + 1              -- odd id → next position (agar exist kare)
         ELSE id                      -- last odd id as it is
     END AS id,
