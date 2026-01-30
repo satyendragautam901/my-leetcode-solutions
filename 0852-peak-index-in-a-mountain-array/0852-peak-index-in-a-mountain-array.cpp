77004f91792297eebcc5aa3a51218c05e402120f
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2026-01-30 18:47:46 +0530

    Time: 0 ms (100%), Space: 63.6 MB (46.65%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& nums) {
-        int n = nums.size(); 
+       int n = nums.size(); 
         int peak = 0;
         for(int i = 1; i<n; i++){
             if(nums[i] > nums[i-1]){
@@ -9,6 +9,6 @@ public:
             }
         }
 
-        return peak;
+        return peak; 
     }
 };
