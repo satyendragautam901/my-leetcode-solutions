f41a65ecca7ebe2d704ad1afe1506e339d2874dd
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2026-02-15 18:17:37 +0530

    Time: 0 ms (100%), Space: 9 MB (36.83%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result = "";
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0'; // convert char to int
+            if (j >= 0) sum += b[j--] - '0';
+
+            result += (sum % 2) + '0'; // add current bit to result
+            carry = sum / 2;           // update carry
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
