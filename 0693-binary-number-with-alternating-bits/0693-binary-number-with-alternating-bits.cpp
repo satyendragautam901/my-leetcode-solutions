2529ae0a240b0f20935bf6752e37a5ebd435155f
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2026-02-18 11:31:30 +0530

    Time: 0 ms (100%), Space: 7.7 MB (91.43%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        // bitset is used to store binary representation of any number
+        // first store current bit
+        int currBit = n % 2;
+        n = n/2;
 
-        bitset<32> bi(n);
-
-        int highest = log2(n); // highest set bits
-        for(int i = 0; i<=highest; i++){
-            cout<<bi[i]<<" "<<bi[i+1]<<endl;
-            if(bi[i] == bi[i+1])
+        while(n>0){
+            if(currBit == n%2){
                 return false;
+            }
+            currBit = n%2;// update current bit
+            n = n/2;
         }
+
         return true;
     }
 };
