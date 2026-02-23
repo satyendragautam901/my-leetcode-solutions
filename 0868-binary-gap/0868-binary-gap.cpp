ae24fc7f1ada99f7289d1f0afb71c4873d2b5b85
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2026-02-23 11:57:04 +0530

    Time: 0 ms (100%), Space: 7.9 MB (94.71%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.cpp
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -5,14 +5,12 @@ public:
         int prev = -1;
         int ans = 0;
 
-        while(n>0){
-            if((n&1) > 0){ // means last bit 1 rha hoga
-                ans = (prev != -1) ? max(ans , curr - prev) : ans;
-                prev = curr;
+        for(int i = curr ; i<32; i++){
+            if(((n >> i) & 1) > 0){ // means last bit 1 rha hoga
+                ans = (prev != -1) ? max(ans , i - prev) : ans;
+                prev = i;
             }
-            curr++;
-            n = n>>1;
-        }
+                    }
 
         return ans;
     }
