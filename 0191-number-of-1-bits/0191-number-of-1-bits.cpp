d2246dabe4c83c5f18e14b600d8ee5d04a92940d
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2026-03-18 22:07:27 +0530

    Time: 0 ms (100%), Space: 8.4 MB (11.18%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int cnt = 0;
+        while(n!=0){
+            if(n%2 != 0)
+                cnt++;
+           
+                n = n/2;
+        }
+
+        return cnt;
+    }
+};
