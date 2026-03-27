dc5b4fd214cbeb117eb5e2e83fe968f7eb5fbc72
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2026-03-27 22:26:31 +0530

    Time: 0 ms (100%), Space: 31.3 MB (44.8%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -6,17 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
+    int getHeight(TreeNode* root) {
+        int h = 0;
+        while (root) {
+            h++;
+            root = root->left;
+        }
+        return h;
+    }
 
     int countNodes(TreeNode* root) {
-        
-        if(!root)
+        if (!root)
             return 0;
-        // call left first then right
-        return 1 + countNodes(root->left) + countNodes(root->right);
+
+        int leftHeight = getHeight(root->left);
+        int rightHeight = getHeight(root->right);
+
+        if (leftHeight == rightHeight) {
+            // left subtree perfect
+            return (1 << leftHeight) + countNodes(root->right);
+        } else {
+            // right subtree perfect
+            return (1 << rightHeight) + countNodes(root->left);
+        }
     }
 };
