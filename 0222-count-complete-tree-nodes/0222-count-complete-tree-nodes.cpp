4473d7aed86ef4c0f7eebeac2c767bfc9ebccbab
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2026-04-03 14:39:39 +0530

    Time: 0 ms (100%), Space: 31.2 MB (73.57%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -5,10 +5,10 @@
  *     TreeNode *left;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *  ////   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
  * right(right) {}
- * };/////
+ * };////
  */
 class Solution {
 public:
