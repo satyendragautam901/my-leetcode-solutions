605c2a21e446950c1951f6c8f71709018f172be5
Satyendra Gautam <142148319+satyendragautam901@users.noreply.github.com>
2026-04-22 22:49:12 +0530

    Time: 0 ms (100%), Space: 12.3 MB (97.33%) - LeetHub

---
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> twoEditWords(vector<string>& queries, vector<string>& dictionary) {
+        int n = queries.size();
+        vector<string>ans;
+
+        for (string& query : queries) {
+            
+            for (string& s : dictionary) {
+                int diff = 0;
+
+                for (int i = 0; i < query.size(); i++) {
+                    if (query[i] != s[i]) {
+                        ++diff;
+                    }
+
+                    if(diff > 2)
+                        break;
+                }
+
+                if (diff <= 2) {
+                    ans.push_back(query);
+                    break;
+                }
+            }
+
+        }
+            return ans;
+    }
+};
